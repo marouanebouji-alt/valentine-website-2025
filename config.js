04e03ec44aec627d193ee0a0ef30de8eff5f8262
marouanebouji-alt <marouanebouji@gmail.com>
2026-02-12 20:16:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -68,3 +68,5 @@ music: {
     stopText: "🔇 Stop Music", // Button text to stop music
     volume: 0.5 // Volume level (0.0 to 1.0)
 }
+// Don't modify anything below this line unless you know what you're doing
+window.VALENTINE_CONFIG = CONFIG; 
