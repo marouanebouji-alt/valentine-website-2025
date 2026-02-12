ebb44c68fae4c2dd2fd1b64c21867240a15c8cb6
marouanebouji-alt <marouanebouji@gmail.com>
2026-02-12 20:30:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Basic Information
-valentineName: "Carla"                    // Your Valentine's name
-pageTitle: "Veut-tu etre ma valentine? 💝" // Browser tab title
+valentineName: "Jade"                    // Your Valentine's name
+pageTitle: "Will You Be My Valentine? 💝" // Browser tab title
 
 // Floating Background Elements
 floatingEmojis: {
@@ -11,34 +11,34 @@ floatingEmojis: {
 // Questions and Buttons
 questions: {
     first: {
-        text: "Est ce que tu m'apprecies?",                   // First question
-        yesBtn: "OUIII",                             // Yes button text
-        noBtn: "NONNN BEURK",                               // No button text
-        secretAnswer: "Je ne t'apprecie pas mais JE T'AIME COMME JAJA ❤️🫶🏻"  // Hidden message
+        text: "Do you like me?",                   // First question
+        yesBtn: "Yes",                             // Yes button text
+        noBtn: "No",                               // No button text
+        secretAnswer: "I don't like you, I love you! ❤️"  // Hidden message
     },
     second: {
-        text: "Comment tu m'aimes?",          // Second question
-        startText: "C'est pas mal sa!",                   // Text before percentage
+        text: "How much do you love me?",          // Second question
+        startText: "This much!",                   // Text before percentage
         nextBtn: "Next ❤️"                         // Next button text
     },
     third: {
-        text: "Veut-tu etre ma Valentine...?",      // Final question
-        yesBtn: "Ouiii!",                            // Yes button text
-        noBtn: "Non toz"                                // No button text
+        text: "Will you be my Valentine...?",      // Final question
+        yesBtn: "Yes!",                            // Yes button text
+        noBtn: "No"                                // No button text
     }
 }
 
 // Love Meter Messages
 loveMessages: {
-    extreme: "WOOOOW Tu m'aimes autant ?? 🥰",  // Shows above 5000%
-    high: "C'EST COMBIEN LA DOT ?! 🚀💝",              // Shows above 1000%
-    normal: "Attend que je te passe la bague au doigt ma puce! 🥰"                           // Shows above 100%
+    extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows above 5000%
+    high: "To infinity and beyond! 🚀💝",              // Shows above 1000%
+    normal: "And beyond! 🥰"                           // Shows above 100%
 }
 
 // Final Celebration
 celebration: {
-    title: "La fete ! Je suis chanceux...",     // Celebration title
-    message: "Le meilleur reste a venir...",          // Celebration message
+    title: "Yay! I'm the luckiest person...",     // Celebration title
+    message: "Now come get your gift...",          // Celebration message
     emojis: "🎁💖🤗💝💋❤️💕"                        // Celebration emojis
 }
 
@@ -63,7 +63,7 @@ animations: {
 music: {
     enabled: true, // Music feature is enabled
     autoplay: true, // Try to autoplay (note: some browsers may block this)
-    musicUrl: "https://on.soundcloud.com/QtZFqRSRad3XGJVXzw", // Paste your music URL here
+    musicUrl: "YOUR_CLOUDINARY_URL_HERE", // Paste your music URL here
     startText: "🎵 Play Music", // Button text to start music
     stopText: "🔇 Stop Music", // Button text to stop music
     volume: 0.5 // Volume level (0.0 to 1.0)
